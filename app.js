52ec1c006715d8e4cb9816befde2c81c958be421
rajantha22 <hncrasteam@gmail.com>
2021-12-28 00:34:57 +0530

    data status commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const weatherData = require('./routes/weather/weatherData')
 const station = require('./routes/station/station')
+const data = require('./routes/data/data')
 const app = express();
 
 app.use(express.json())
@@ -10,6 +11,7 @@ app.use(express.json())
 //routes
 app.use('/update',weatherData);
 app.use('/stations',station)
+app.use('/data',data)
 
 //    /weatherstation/updateweatherstation [post]
 //    /waterlevelgauge/updatewaterlevelgauge [post]
